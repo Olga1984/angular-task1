7ce9b7b68f79c84c4d451c6efe0358040f3d6d19
Heorhi Shakanau <g.shokanov@gmail.com>
2019-02-25 11:27:10 +0300

    add build scripts

---
+++ gulp/build.js
@@ -0,0 +1,43 @@
+const gulp = require('gulp');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const sourceMaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const cleanCss = require('gulp-clean-css');
+const inject = require('gulp-inject');
+
+sass.compiler = require('node-sass');
+
+function compileJs() {
+    return browserify('../src/index.js')
+        .transform('babelify', {presets: ['@babel/preset-env']})
+        .bundle()
+        .pipe(source('main.bundle.js'))
+        .pipe(buffer())
+        .pipe(sourceMaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .pipe(sourceMaps.write())
+        .pipe(gulp.dest('../dist'));
+}
+
+function compileSass() {
+    return gulp.src('../src/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCss())
+        .pipe(gulp.dest('../dist'));
+}
+
+function injectBundles() {
+    const sources = gulp.src(['../dist/*.js', '../dist/*.css'], {read: false});
+    return gulp.src('../src/index.html')
+        .pipe(inject(sources), { removeTags: true })
+        .pipe(gulp.dest('../dist'));
+}
+
+module.exports = {
+    compileJs,
+    compileSass,
+    injectBundles
+}
