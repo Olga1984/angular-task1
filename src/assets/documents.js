14f6562775f218450422c2f27939870219c6ec66
Heorhi Shakanau <g.shokanov@gmail.com>
2019-02-25 13:17:33 +0300

    add basic documentService

---
+++ src/assets/documents.js
@@ -0,0 +1,32 @@
+export const mockDocuments = [
+    {
+        headline: 'Something happend',
+        text: 'Something really important happened somewhere today',
+        views: 123
+    },
+    {
+        headline: 'Something happend',
+        text: 'Something really important happened somewhere today',
+        views: 412
+    },
+    {
+        headline: 'Something happend',
+        text: 'Something really important happened somewhere today',
+        views: 654
+    },
+    {
+        headline: 'Something happend',
+        text: 'Something really important happened somewhere today',
+        views: 87
+    },
+    {
+        headline: 'Something happend',
+        text: 'Something really important happened somewhere today',
+        views: 90132
+    },
+    {
+        headline: 'Something happend',
+        text: 'Something really important happened somewhere today',
+        views: 1241
+    }
+]
