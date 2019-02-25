46503b43a1df9134a2b317d7f9691aae820c1c06
OLga1984 <noa84@tut.by>
2019-02-25 17:19:36 +0300

    add sidebar generation

---
+++ src/assets/sidebarItems.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        sidebarItemTitle: 'Taxes',
+        count: 4,
+    },
+    {
+        sidebarItemTitle: 'News',
+        count: 5,
+    },
+    {
+        sidebarItemTitle: 'Topics',
+        count: 3,
+    },
+    {
+        sidebarItemTitle: 'Journals',
+        count: 0,
+    },
+];
