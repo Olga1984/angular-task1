9fc4ffe7a38fca94a4e5e6af0e46f3071d526906
Heorhi Shakanau <g.shokanov@gmail.com>
2019-02-26 12:30:22 +0300

    add searchbar controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,1 +1,2 @@
-export { default } from './DocumentController';
+export { default as DocumentController } from './DocumentController';
+export { default as SearchbarController } from './SearchbarController';
