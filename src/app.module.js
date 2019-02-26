f560bb5043e9ebfeb9bf6b19445414da8f683bd2
OLga1984 <noa84@tut.by>
2019-02-26 14:16:47 +0300

    add article directive

--- src/app.module.js
+++ src/app.module.js
@@ -2,3 +2,4 @@ import angular from 'angular';
 
 angular.module('document', ['searchbar']);
 angular.module('searchbar', ['templates']);
+angular.module('article', ['templates']);
---
+++ src/directives/articleDirective.js
@@ -0,0 +1,9 @@
+import angular from 'angular';
+
+export default angular.module('article')
+    .directive('ngArticle', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'article.html',
+        }
+    });
