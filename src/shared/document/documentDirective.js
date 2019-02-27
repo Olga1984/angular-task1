bf2a8d688ea87cad87d76bf5818eaffbbd3de863
Heorhi Shakanau <g.shokanov@gmail.com>
2019-02-27 15:00:40 +0300

    add document component

---
+++ src/shared/document/documentDirective.js
@@ -0,0 +1,13 @@
+import angular from 'angular';
+
+export default angular.module('shared')
+    .directive('swtDocument', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'document.html',
+            scope: {
+                showHeadline: '@headline',
+                document: '='
+            }
+        }
+    });
