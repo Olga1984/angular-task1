e0978ea6fd0ba21ca47d5d43cb41ddd7791ca3e3
Heorhi Shakanau <g.shokanov@gmail.com>
2019-02-27 15:00:11 +0300

    refactor documentService and remove redundant methods

---
+++ src/shared/services/documentService.js
@@ -0,0 +1,16 @@
+import angular from 'angular';
+import {mockDocuments} from '../../assets/documents';
+
+export default angular.module('shared')
+    .service('documentService', ['$q', function documentService($q) {
+        const documents = [];
+
+        function getDocuments() {
+            return $q.resolve(mockDocuments);
+        }
+        return {
+            documents,
+            getDocuments,
+        };
+    }]);
+
