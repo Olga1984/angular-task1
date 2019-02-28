10ae94d68a9722baa08b8c5e869f81dbc953f2e7
Heorhi Shakanau <g.shokanov@gmail.com>
2019-02-28 11:58:05 +0300

    add searchPage module

--- src/app.module.js
+++ src/app.module.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 
 angular.module('core', ['templates', 'searchPage', 'shared']);
+angular.module('searchPage', []);
 angular.module('shared', []);
 angular.module('searchPage', []);
