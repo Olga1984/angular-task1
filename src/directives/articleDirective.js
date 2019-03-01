c92c460c7c52531f46355d4e3e524e9ecaf67736
Heorhi Shakanau <g.shokanov@gmail.com>
2019-03-01 11:33:06 +0300

    remove redundant code

--- src/directives/articleDirective.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-
-export default angular.module('article')
-    .directive('ngArticle', function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'article.html',            
-        }
-    });
--- src/directives/index.js
+++
@@ -1,2 +0,0 @@
-import './searchbarDirective';
-import './articleDirective';
