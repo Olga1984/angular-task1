b4b646f5678dfe5b56c02db4b4123fb89c9066ac
Heorhi Shakanau <g.shokanov@gmail.com>
2019-03-05 12:02:11 +0300

    refactor mockDocuments

--- src/assets/documents.js
+++ src/assets/documents.js
@@ -2,7 +2,7 @@ export const mockDocuments = [{
     category: 'Journals',
     value: {
         headline: 'Journals happend',
-        text: 'Something really important happened somewhere today Something really important happened somewhere today Something really important happened somewhere today',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ipsum vel tortor aliquam eleifend. Sed non velit rhoncus quam volutpat gravida tincidunt nec lectus. Nullam tristique erat a lacus sollicitudin, commodo tempus elit viverra. Vestibulum imperdiet neque nec tortor mollis, at scelerisque nisi varius. Vivamus ligula magna, porta et felis non, convallis facilisis est. Morbi ut nunc libero. Vivamus posuere auctor sem, quis molestie massa laoreet vitae. Donec nec libero vitae turpis aliquet scelerisque ac ac dui. Praesent metus turpis, elementum eu purus faucibus, imperdiet pellentesque nisi. Duis maximus libero dictum mi vulputate, in iaculis neque commodo.',
         views: 90132,
     },
 },
@@ -18,7 +18,7 @@ export const mockDocuments = [{
     category: 'News',
     value: {
         headline: 'Something News',
-        text: 'Something really important happened somewhere today Something really important happened somewhere today Something really important happened somewhere today',
+        text: 'Etiam sit amet malesuada dolor. Suspendisse eu dignissim mauris. Aliquam ut leo dignissim, posuere nunc non, ullamcorper neque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras nec odio in massa fermentum pellentesque vehicula eget odio. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris ligula turpis, vehicula id justo non, laoreet maximus augue. Nam vel ex ullamcorper, posuere magna eu, consequat orci. Suspendisse accumsan ligula augue, convallis posuere libero sodales ac. Vivamus eget ipsum in velit gravida faucibus consectetur id dolor. Etiam et dui sed leo congue tristique. Curabitur non lobortis nibh, sit amet fringilla nisl. Maecenas et sem ut lorem consequat aliquam in ut lectus. In gravida quam dolor, eget porta lorem vulputate at.',
         views: 932,
     },
 },
@@ -26,7 +26,7 @@ export const mockDocuments = [{
     category: 'Taxes',
     value: {
         headline: 'Taxes happend',
-        text: 'Something really important happened somewhere today Something really important happened somewhere today Something really important happened somewhere today',
+        text: 'Maecenas malesuada at ipsum ultrices elementum. Sed dolor odio, mollis nec ligula sodales, semper sollicitudin libero. Phasellus venenatis, nulla sed interdum auctor, orci metus congue dui, et feugiat ex enim sed nunc. Donec malesuada, leo quis consequat efficitur, nulla velit condimentum enim, sed tristique sapien nunc rutrum nunc. In quis odio ac ipsum feugiat viverra. Etiam odio est, tristique eget pharetra sed, tincidunt eget purus. Nullam non efficitur nulla. Suspendisse diam erat, semper sed rhoncus vitae, lacinia in odio.',
         views: 32,
     },
 },
@@ -42,7 +42,7 @@ export const mockDocuments = [{
     category: 'Taxes',
     value: {
         headline: 'Taxes happend',
-        text: 'Something really important happened somewhere today Something really important happened somewhere today Something really important happened somewhere today',
+        text: 'Vestibulum ac malesuada neque, at porta nisi. Nam urna sapien, egestas vel eros nec, efficitur cursus quam. Duis sed mauris egestas, convallis felis sed, hendrerit ex. Nullam fringilla finibus mi, eget euismod felis euismod a. Nullam blandit eget justo nec cursus. Suspendisse orci tortor, gravida ac convallis nec, mollis eget dolor. Sed non leo eu nunc egestas fermentum a vel arcu. Sed volutpat a nibh eu vestibulum.',
         views: 90,
     },
 },
@@ -50,7 +50,7 @@ export const mockDocuments = [{
     category: 'News',
     value: {
         headline: 'News happend',
-        text: 'Something really important happened somewhere today Something really important happened somewhere today Something really important happened somewhere today',
+        text: 'Nullam condimentum ac risus quis vulputate. Fusce at lorem quis lectus malesuada semper eget sed massa. Duis ut est ante. In id turpis a mi fringilla convallis quis vel sapien. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean nibh sem, dictum et tempor vel, rhoncus ut ipsum. Vestibulum sit amet enim arcu. Cras sapien neque, pulvinar eu egestas at, molestie non enim.',
         views: 132,
     },
 },
@@ -58,7 +58,7 @@ export const mockDocuments = [{
     category: 'Topics',
     value: {
         headline: 'Topics happend',
-        text: 'Something really important happened somewhere today Something really important happened somewhere today Something really important happened somewhere today',
+        text: 'Pellentesque ac nisi ultricies, accumsan ipsum vel, volutpat metus. Pellentesque elementum velit quam, vitae semper magna venenatis a. Fusce nulla lorem, mollis sit amet iaculis et, mattis id mi. Morbi enim velit, dignissim semper dignissim nec, tincidunt vel justo. Maecenas bibendum efficitur eros, feugiat volutpat lacus sodales vitae. Proin faucibus nisl non nisl consectetur aliquam. Mauris placerat leo laoreet, tempor turpis scelerisque, iaculis leo. Vestibulum quis ipsum at tellus accumsan aliquet vitae eu leo. Proin rhoncus dolor faucibus, elementum justo nec, sodales diam. Integer eu velit condimentum, iaculis lorem sed, tristique quam. Nullam ultricies sagittis neque eleifend facilisis. Proin quis laoreet augue, nec molestie risus. Nam quis bibendum ipsum, quis sollicitudin massa.',
         views: 701,
     },
 },
@@ -74,7 +74,7 @@ export const mockDocuments = [{
     category: 'News',
     value: {
         headline: 'News happend',
-        text: 'Something really important happened somewhere today Something really important happened somewhere today Something really important happened somewhere today',
+        text: 'Aliquam consectetur augue turpis, vel tristique ipsum tincidunt ac. Ut a dignissim est. Aenean at posuere turpis. Duis quis luctus purus. Aenean efficitur vestibulum tortor vel hendrerit. Morbi sit amet pharetra ipsum, at eleifend erat. Sed lacinia metus sapien, vel consequat justo fringilla et. Suspendisse ac velit et quam facilisis volutpat. Cras at odio tellus. Mauris quam lectus, bibendum sed lacinia sed, ultrices nec dolor. Donec ut ex eget dolor lacinia lacinia quis eu nisl. Proin luctus maximus vestibulum. Quisque a felis nec nunc egestas pellentesque in non quam. Nam lacinia rhoncus velit sed interdum. Suspendisse nunc urna, tristique id sem a, condimentum laoreet risus.',
         views: 132,
     },
 },
@@ -90,7 +90,7 @@ export const mockDocuments = [{
     category: 'Topics',
     value: {
         headline: 'Something Topics',
-        text: 'Something really important happened somewhere today Something really important happened somewhere today Something really important happened somewhere today',
+        text: 'Donec ut elementum erat, at ullamcorper est. Ut tristique, mauris in interdum consectetur, neque leo viverra leo, non sagittis enim leo vitae urna. Cras eros turpis, dignissim at scelerisque non, fringilla sed odio. Morbi tristique, justo sit amet sodales vestibulum, justo elit dapibus tellus, eget sagittis odio libero non arcu. Nunc convallis ipsum a maximus fermentum. Nam tincidunt cursus ultrices. In ac cursus risus.',
         views: 90132,
     },
 },
@@ -98,7 +98,7 @@ export const mockDocuments = [{
     category: 'News',
     value: {
         headline: 'News happend',
-        text: 'Something really important happened somewhere today Something really important happened somewhere today Something really important happened somewhere today',
+        text: 'Sed iaculis, lectus vitae mollis consectetur, nibh libero interdum ipsum, eu pulvinar arcu leo eu risus. Proin pulvinar eros scelerisque lorem dignissim, pretium blandit nisi volutpat. Etiam sit amet elit vitae lectus elementum laoreet. Morbi posuere ex eros, non ultrices purus condimentum sagittis. Duis eget auctor orci. Morbi ultrices lobortis neque et iaculis. In non felis aliquet, finibus mi vel, ultricies diam. Ut id hendrerit urna. Nam iaculis dolor non elit sodales sodales. Pellentesque convallis, lorem sit amet sagittis ultrices, risus eros blandit lacus, volutpat vestibulum felis nibh eget enim. Etiam porta maximus ligula sit amet euismod. Nullam aliquam bibendum leo. Pellentesque tempus arcu vel odio maximus rutrum. Etiam id convallis libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec nisl nulla, lacinia vel sagittis nec, pulvinar finibus augue.',
         views: 132,
     },
 },
@@ -106,7 +106,7 @@ export const mockDocuments = [{
     category: 'Topics',
     value: {
         headline: 'Topics happend',
-        text: 'Something really important happened somewhere today Something really important happened somewhere today Something really important happened somewhere today',
+        text: 'Ut nisl justo, sagittis ac urna non, convallis gravida tortor. Fusce in tristique libero. Sed porta commodo nisl, ac pellentesque metus pellentesque a. In vel urna velit. Donec faucibus molestie nulla, vel ultrices ipsum efficitur eu. Morbi varius turpis ac enim ullamcorper ultrices. Nam ornare arcu sed sollicitudin facilisis. Praesent velit enim, luctus ut semper a, ullamcorper sed justo. Cras ligula quam, aliquet sit amet bibendum a, fermentum eu ante. Donec suscipit eleifend metus, et luctus elit cursus ac. Aenean interdum nunc ac imperdiet molestie. Vivamus volutpat tortor cursus magna vestibulum tincidunt. Curabitur tincidunt, dui ut cursus malesuada, diam mi efficitur tortor, sit amet dictum augue dui vel mauris.',
         views: 701,
     },
 },
