94bca672ccbcb3faea7ac9fe45a23049a44a4c0d
Heorhi Shakanau <g.shokanov@gmail.com>
2019-03-05 17:30:46 +0300

    add document search to documentService

--- src/shared/services/documentService.js
+++ src/shared/services/documentService.js
@@ -4,13 +4,16 @@ import {mockDocuments} from '../../assets/documents';
 export default angular.module('shared')
     .service('documentService', ['$q', function documentService($q) {
         const documents = [];
-
         function getDocuments() {
+            return $q.resolve(mockDocuments);
+        }
+        function searchDocuments(query) {
             return $q.resolve(mockDocuments);
         }
         return {
             documents,
             getDocuments,
+            searchDocuments,
         };
     }]);
 
