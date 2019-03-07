693939d6877b4729729f8c3b0c64e1c85e3453f0
Olga1984 <noa84@tut.by>
2019-03-07 08:03:47 +0300

    update data loading

--- src/translate/translateController.js
+++ src/translate/translateController.js
@@ -7,12 +7,12 @@ export default angular.module('translate')
             if (!url) url = window.location.href;
             name = name.replace(/[\[\]]/g, '\\$&');
             let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
-                var results = regex.exec(url);
+                const results = regex.exec(url);
             if (!results) return null;
             if (!results[2]) return '';
             return decodeURIComponent(results[2].replace(/\+/g, ' '));
         };
-        // If a parameter is present several times (?locale=de-DEfoo=ipsum), you will get the first value (de-DE)
+        // If a parameter is present several times (?locale=de-DE&foo=ipsum), you will get the first value (de-DE)
         // http://localhost:3000/?en-US
         $translate.use(this.getParameterByName('locale'));
 
