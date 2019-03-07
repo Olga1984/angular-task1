1fe2900c6e4179ccd8b9ca634552e117be1c68c1
Olga1984 <noa84@tut.by>
2019-03-07 09:09:04 +0300

    add active button style

--- src/translate/translateController.js
+++ src/translate/translateController.js
@@ -2,12 +2,13 @@ import angular from 'angular';
 
 export default angular.module('translate')
     .controller('TranslateController', ['$translate', function ($translate) {
+        this.selectedLanguage = 'en';
         // https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript?page=1&tab=votes#tab-top
         this.getParameterByName = (name, url) => {
             if (!url) url = window.location.href;
             name = name.replace(/[\[\]]/g, '\\$&');
-            let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
-                const results = regex.exec(url);
+            const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`);
+            const results = regex.exec(url);
             if (!results) return null;
             if (!results[2]) return '';
             return decodeURIComponent(results[2].replace(/\+/g, ' '));
@@ -16,7 +17,9 @@ export default angular.module('translate')
         // http://localhost:3000/?en-US
         $translate.use(this.getParameterByName('locale'));
 
+
         this.changeLanguage = (key) => {
             $translate.use(key);
+            this.selectedLanguage = key;
         };
     }]);
