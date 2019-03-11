649f5f4407d1004cdbf5554abc97082b31272da8
OLga1984 <noa84@tut.by>
2019-03-11 12:32:25 +0300

    add log handler extension

--- src/translate/translateConfig.js
+++ src/translate/translateConfig.js
@@ -5,20 +5,18 @@ import es from '../assets/data/es.json';
 
 export default angular.module('translate')
     .config(['$translateProvider', ($translateProvider) => {
+        $translateProvider.preferredLanguage('en');
+        $translateProvider.useMissingTranslationHandlerLog();
         $translateProvider.translations('en', en);
         $translateProvider.translations('de', de);
         $translateProvider.translations('es', es);
         $translateProvider.useCookieStorage();
         $translateProvider.useLocalStorage();
-        $translateProvider.preferredLanguage('en');
+
         $translateProvider.registerAvailableLanguageKeys(['en', 'de', 'es'], {
             'en-*': 'en',
             'de-*': 'de',
             'es-*': 'es',
-        });
-        $translateProvider.useStaticFilesLoader({
-            prefix: 'data/',
-            suffix: '.json',
         });
         $translateProvider.useSanitizeValueStrategy('sanitize');
     }]);
