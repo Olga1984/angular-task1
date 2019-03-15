a59aaca51dc47e2c23ec6de46c2b457bdc8657dc
Heorhi Shakanau <g.shokanov@gmail.com>
2019-03-15 16:36:40 +0300

    add controller to searchNavDirectove

--- client/src/searchPage/searchNav/searchNavDirective.js
+++ client/src/searchPage/searchNav/searchNavDirective.js
@@ -4,6 +4,8 @@ export default angular.module('searchPage')
     .directive('swtSearchNav', () => (
         {
             templateUrl: 'searchNav.html',
+            controller: 'SearchNavController',
+            controllerAs: 'searchNav',
             scope: {
                 activePage: '<',
                 paginatedDocuments: '<',
