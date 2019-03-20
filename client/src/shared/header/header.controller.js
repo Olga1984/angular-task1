24a5055279f25c187520b748c5d3b2ecc11dc4bd
OLga1984 <noa84@tut.by>
2019-03-20 17:59:44 +0300

    simplify logo

--- client/src/shared/header/header.controller.js
+++ client/src/shared/header/header.controller.js
@@ -3,7 +3,6 @@ import angular from 'angular';
 const sharedModule = angular.module('shared');
 sharedModule.controller('HeaderController', function HeaderController() {
     this.menuActive = false;
-    this.state = 'home';
     this.toggleMenu = () => {
         this.menuActive = !this.menuActive;
     };
