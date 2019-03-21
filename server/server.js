d698cee2b83818d9563d2591fff930f9e65892ac
Heorhi Shakanau <g.shokanov@gmail.com>
2019-03-21 11:03:13 +0300

    rewrite server middleware

--- server/server.js
+++ server/server.js
@@ -18,36 +18,41 @@ server.use(middlewares);
 server.use(jsonServer.bodyParser);
 server.use(jsonServer.rewriter({
     "/api/documents/popular": "/documents?_sort=popular&_order=desc&_limit=8",
-    "/api/documents?search=:search": "/documents?q=:search",
-    "/api/documents?search=:search&filterId=:filter": "/documents?q=:search&filterId=:filter",
     "/api/*": "/$1"
 }));
 
 
-server.use((req, res, next) => {
+server.use('/documents', (req, res, next) => {
     const { documents } = data;
-    const { q: searchQuery, filtersCount } = req.query;
-    if (typeof filtersCount !== 'string') {
+    const { search, filterId, filtersCount } = req.query;
+    if (!search) {
         return next();
     }
-    if (filtersCount !== 'true') {
-        return res.sendStatus(400);
+    const result = {
+        documents: [],
+    };
+    const regex = new RegExp(search, 'i');
+    const filtersArr = Array.isArray(filterId)
+    ? filterId.map(Number)
+    : [Number(filterId)];
+    if (filtersCount === 'true') {
+        result.filtersCount = {};
     }
-    const cache = Object.create(null);
-    const regex = new RegExp(searchQuery, 'i');
-    documents.forEach((elem) => {
-        const { filterId, headline, text } = elem;
+    documents.forEach((document) => {
+        const { filterId: documentFilter, headline, text } = document;
         if (!regex.test(headline) && !regex.test(text)) {
             return;
         }
-        if (cache[filterId]) {
-            cache[filterId] += 1;
-        } else {
-            cache[filterId] = 1;
+        if (filtersCount === 'true') {
+            result.filtersCount[documentFilter] = result.filtersCount[documentFilter] + 1 || 1;
         }
+        if (filterId && !filtersArr.includes(documentFilter)) {
+            return;
+        }
+        result.documents.push(document);
     });
-    res.jsonp(cache);
-})
+    res.jsonp(result);
+});
 
 server.use(router);
 
