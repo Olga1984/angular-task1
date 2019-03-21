5953ffb633538c612ed006bd67889686a808792d
OLga1984 <noa84@tut.by>
2019-03-21 10:50:21 +0300

    add new lines before include and remove unnecessry file and translate function

--- client/src/translate/translateController.js
+++ client/src/translate/translateController.js
@@ -2,8 +2,6 @@ import angular from 'angular';
 
 export default angular.module('translate')
     .controller('TranslateController', ['$translate', function ($translate) {
-        this.selectedLanguage = $translate.use();
-
         this.changeLanguage = (key) => {
             $translate.use(key);
             this.selectedLanguage = key;
